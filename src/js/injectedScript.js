26450846c0f85ee03c76df9eaf9f735c5cd1bdaf
wangweihua <aztack@163.com>
2018-08-06 07:43:21 +0800

    add createDebugLayer

--- src/js/injectedScript.js
+++ src/js/injectedScript.js
@@ -11,14 +11,29 @@ export default function () {
     'rotation', 'rotationX', 'rotationY',
     'scale', 'scaleX', 'scaleY',
     'skewX', 'skewY'
-  ]
+  ];
+
+  const NodesCache = {};
+
   const ccdevtool = window.ccdevtool = {
+    NodesCache,
     getTreeNodes () {
       const scene = cc.director.getScene();
       return this.serialize(scene);
     },
     postMessage (type, data) {
       window.postMessage({type, data}, '*');
+    },
+    createDebugLayer () {
+      const debugLayer = document.createElement('div');
+      debugLayer.id = 'cc-devtool-debug';
+      const s = debugLayer.style;
+      s.position = 'absolute';
+      s.top = s.bottom = s.left = s.right = 0;
+
+      const ctn = document.querySelector('#Cocos2dGameContainer');
+      ctn.position = 'relative';
+      ctn.appendChild(debugLayer);
     },
     serialize: function serialize (n) {
       const kv = SerializeProps.reduce((result, key) => {
@@ -27,7 +42,11 @@ export default function () {
         result.push({key, value});
         return result;
       }, []);
-      return {
+      const box = n.convertToNodeSpaceAR(cc.v2(0, 0));
+      box.width = n.width;
+      box.height = n.height;
+      kv.box = box;
+      return NodesCache[n.uuid] = {
         label: n.name,
         props: kv,
         children: n.children.map(serialize)
